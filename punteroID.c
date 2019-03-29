e8600ec70164a67e9f19da012adbe5fd3b824823
mmartc13 <47885852+mmartc13@users.noreply.github.com>
2019-03-29 12:18:06 +0100

    Add files via upload

---
+++ punteroID.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	int n;
+	int*v;
+	int i;
+	int min;
+	printf("dame un numero\n");
+	scanf("%d",&n);
+	v=(int *)malloc(n*sizeof(int));
+	for(i=0;i<n;i++)
+	{
+	scanf("%d",&*(v+i));
+	}
+	min=*(v+0);
+	
+	for(i=1;i<n;i++)
+	{
+		if(*(v+i)<min)
+		min=*(v+i);
+		}
+		printf("%d",min);
+		free(v);
+			
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
